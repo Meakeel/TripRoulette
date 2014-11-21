5d22f19f589a750279eca0771accdfd502fcdca8
Paul Sparkes <paul@objectivedesign.co.uk>
2014-11-21 08:29:32 +0000

    Inital Commit
    Signed-off-by: Paul Sparkes <paul@objectivedesign.co.uk>

---
+++ TripRoulette.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TripRoulette.Web.MvcApplication" Language="C#" %>
---
+++ TripRoulette.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripRoulette.Web.Startup))]
+namespace TripRoulette.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
