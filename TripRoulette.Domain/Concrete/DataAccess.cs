06c3ffa9a52c51a0153e449a3133cbbdeaf4f04e
Paul Sparkes <paul@objectivedesign.co.uk>
2014-11-22 16:39:04 +0000

    Signed-off-by: Paul Sparkes <paul@objectivedesign.co.uk>

--- TripRoulette.Domain/Concrete/DataAccess.cs
+++ TripRoulette.Domain/Concrete/DataAccess.cs
@@ -15,66 +15,71 @@ namespace TripRoulette.Domain.Concrete
 
         public Event Get_Event(int id)
         {
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
-            sqlConnection.Open();
-            string strSqlQry = "select top 1 * from Event where eventID=" + id.ToString();
-            SqlCommand cmd = new SqlCommand(strSqlQry, sqlConnection);
-
             Event eventrow = new Event();
-
-            SqlDataReader r = cmd.ExecuteReader();
-            while (r.Read())
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
             {
-                eventrow.EventID = Convert.ToInt32(r["eventID"]);
-                eventrow.Name = Convert.ToString(r["name"]);
-                eventrow.StartDate = Convert.ToDateTime(r["startDate"]);
-                eventrow.EndDate = Convert.ToDateTime(r["endDate"]);
-                eventrow.Description = Convert.ToString(r["description"]);
-                eventrow.Price = Convert.ToDecimal(r["price"]);
-                eventrow.LunchPrice = Convert.ToDecimal(r["lunchPrice"]);
-                eventrow.DinnerPrice = Convert.ToDecimal(r["dinnerPrice"]);
-                eventrow.Theme = Convert.ToString(r["theme"]);
-                eventrow.Postcode = Convert.ToString(r["postcode"]);
-                eventrow.Hint = Convert.ToString(r["hint"]);
-                eventrow.FullDetails = Convert.ToString(r["fullDetails"]);
-                eventrow.Duration = Convert.ToInt32(r["duration"]);
-                eventrow.MinPeople = Convert.ToInt32(r["minPeople"]);
-                eventrow.MaxPeople = Convert.ToInt32(r["maxPeople"]);
+                sqlConnection.Open();
+                string strSqlQry = "select top 1 * from Event where eventID=" + id.ToString();
+                SqlCommand cmd = new SqlCommand(strSqlQry, sqlConnection);
+
+
+
+                SqlDataReader r = cmd.ExecuteReader();
+                while (r.Read())
+                {
+                    eventrow.EventID = Convert.ToInt32(r["eventID"]);
+                    eventrow.Name = Convert.ToString(r["name"]);
+                    eventrow.StartDate = Convert.ToDateTime(r["startDate"]);
+                    eventrow.EndDate = Convert.ToDateTime(r["endDate"]);
+                    eventrow.Description = Convert.ToString(r["description"]);
+                    eventrow.Price = Convert.ToDecimal(r["price"]);
+                    eventrow.LunchPrice = Convert.ToDecimal(r["lunchPrice"]);
+                    eventrow.DinnerPrice = Convert.ToDecimal(r["dinnerPrice"]);
+                    eventrow.Theme = Convert.ToString(r["theme"]);
+                    eventrow.Postcode = Convert.ToString(r["postcode"]);
+                    eventrow.Hint = Convert.ToString(r["hint"]);
+                    eventrow.FullDetails = Convert.ToString(r["fullDetails"]);
+                    eventrow.Duration = Convert.ToInt32(r["duration"]);
+                    eventrow.MinPeople = Convert.ToInt32(r["minPeople"]);
+                    eventrow.MaxPeople = Convert.ToInt32(r["maxPeople"]);
+                }
             }
-            return eventrow;
+                return eventrow;
+            
         }
 
         public EventCollection Get_Events()
         {
             EventCollection eventCollection = new EventCollection();
 
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
-            sqlConnection.Open();
-            string strSqlQry = "select * from Event";
-            SqlCommand cmd = new SqlCommand(strSqlQry, sqlConnection);
-
-            SqlDataReader r = cmd.ExecuteReader();
-            while (r.Read())
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
             {
-                Event eventrow = new Event();
-                eventrow.EventID = Convert.ToInt32(r["eventID"]);
-                eventrow.Name = Convert.ToString(r["name"]);
-                eventrow.StartDate = Convert.ToDateTime(r["startDate"]);
-                eventrow.EndDate = Convert.ToDateTime(r["endDate"]);
-                eventrow.Description = Convert.ToString(r["description"]);
-                eventrow.Price = Convert.ToDecimal(r["price"]);
-                eventrow.LunchPrice = Convert.ToDecimal(r["lunchPrice"]);
-                eventrow.DinnerPrice = Convert.ToDecimal(r["dinnerPrice"]);
-                eventrow.Theme = Convert.ToString(r["theme"]);
-                eventrow.Postcode = Convert.ToString(r["postcode"]);
-                eventrow.Hint = Convert.ToString(r["hint"]);
-                eventrow.FullDetails = Convert.ToString(r["fullDetails"]);
-                eventrow.Duration = Convert.ToInt32(r["duration"]);
-                eventrow.MinPeople = Convert.ToInt32(r["minPeople"]);
-                eventrow.MaxPeople = Convert.ToInt32(r["maxPeople"]);
-                eventCollection.Add(eventrow);
-            }
+                sqlConnection.Open();
+                string strSqlQry = "select * from Event";
+                SqlCommand cmd = new SqlCommand(strSqlQry, sqlConnection);
 
+                SqlDataReader r = cmd.ExecuteReader();
+                while (r.Read())
+                {
+                    Event eventrow = new Event();
+                    eventrow.EventID = Convert.ToInt32(r["eventID"]);
+                    eventrow.Name = Convert.ToString(r["name"]);
+                    eventrow.StartDate = Convert.ToDateTime(r["startDate"]);
+                    eventrow.EndDate = Convert.ToDateTime(r["endDate"]);
+                    eventrow.Description = Convert.ToString(r["description"]);
+                    eventrow.Price = Convert.ToDecimal(r["price"]);
+                    eventrow.LunchPrice = Convert.ToDecimal(r["lunchPrice"]);
+                    eventrow.DinnerPrice = Convert.ToDecimal(r["dinnerPrice"]);
+                    eventrow.Theme = Convert.ToString(r["theme"]);
+                    eventrow.Postcode = Convert.ToString(r["postcode"]);
+                    eventrow.Hint = Convert.ToString(r["hint"]);
+                    eventrow.FullDetails = Convert.ToString(r["fullDetails"]);
+                    eventrow.Duration = Convert.ToInt32(r["duration"]);
+                    eventrow.MinPeople = Convert.ToInt32(r["minPeople"]);
+                    eventrow.MaxPeople = Convert.ToInt32(r["maxPeople"]);
+                    eventCollection.Add(eventrow);
+                }
+            }
             return eventCollection;
         }
 
@@ -217,7 +222,8 @@ namespace TripRoulette.Domain.Concrete
 
         public void InsertEvent(Event row)
         {
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
+            { 
             sqlConnection.Open();
 
             SqlCommand cmd = new SqlCommand("insertEvent");
@@ -244,86 +250,91 @@ namespace TripRoulette.Domain.Concrete
             cmd.Parameters.AddWithValue("@lat", lat);
             cmd.Parameters.AddWithValue("@lng", lng);
             cmd.ExecuteNonQuery();
-
+            }
         }
 
         public void InsertEventDetail(EventDetail row)
         {
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
-            sqlConnection.Open();
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
+            {
+                sqlConnection.Open();
 
-            SqlCommand cmd = new SqlCommand("insertEventDetail");
-            cmd.Connection = sqlConnection;
-            cmd.CommandType = CommandType.StoredProcedure;
+                SqlCommand cmd = new SqlCommand("insertEventDetail");
+                cmd.Connection = sqlConnection;
+                cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.AddWithValue("@eventID", row.EventID);
-            cmd.Parameters.AddWithValue("@dayOfWeek", row.DayOfWeek);
-            cmd.Parameters.AddWithValue("@startTime", row.StartTime);
-            cmd.Parameters.AddWithValue("@endTime", row.EndTime);
-            cmd.Parameters.AddWithValue("@allDay", row.allDay);
-            cmd.ExecuteNonQuery();
-
+                cmd.Parameters.AddWithValue("@eventID", row.EventID);
+                cmd.Parameters.AddWithValue("@dayOfWeek", row.DayOfWeek);
+                cmd.Parameters.AddWithValue("@startTime", row.StartTime);
+                cmd.Parameters.AddWithValue("@endTime", row.EndTime);
+                cmd.Parameters.AddWithValue("@allDay", row.allDay);
+                cmd.ExecuteNonQuery();
+            }
         }
 
         public void deleteEvent(int eventId)
         {
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
-            sqlConnection.Open();
-            string strSqlQry = "delete from Event where eventID=" + eventId.ToString();
-            SqlCommand cmd = new SqlCommand(strSqlQry, sqlConnection);
-            cmd.ExecuteNonQuery();
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
+            {
+                sqlConnection.Open();
+                string strSqlQry = "delete from Event where eventID=" + eventId.ToString();
+                SqlCommand cmd = new SqlCommand(strSqlQry, sqlConnection);
+                cmd.ExecuteNonQuery();
+            }
 
         }
 
         public void UpdateEvent(Event row)
         {
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
-            sqlConnection.Open();
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
+            {
+                sqlConnection.Open();
 
-            SqlCommand cmd = new SqlCommand("updateEvent");
-            cmd.Connection = sqlConnection;
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.AddWithValue("@eventID", row.EventID);
-            cmd.Parameters.AddWithValue("@name", row.Name);
-            cmd.Parameters.AddWithValue("@startDate", row.StartDate);
-            cmd.Parameters.AddWithValue("@endDate", row.EndDate);
-            cmd.Parameters.AddWithValue("@description", row.Description);
-            cmd.Parameters.AddWithValue("@price", row.Price);
-            cmd.Parameters.AddWithValue("@lunchPrice", row.LunchPrice);
-            cmd.Parameters.AddWithValue("@dinnerPrice", row.DinnerPrice);
-            cmd.Parameters.AddWithValue("@theme", row.Theme);
-            cmd.Parameters.AddWithValue("@postcode", row.Postcode);
-            cmd.Parameters.AddWithValue("@hint", row.Hint);
-            cmd.Parameters.AddWithValue("@fulldetails", row.FullDetails);
-            cmd.Parameters.AddWithValue("@duration", row.Duration);
-            cmd.Parameters.AddWithValue("@minPeople", row.MinPeople);
-            cmd.Parameters.AddWithValue("@maxPeople", row.MaxPeople);
-            //Google Maps Lang / Lat need to be added.
-            decimal lat = (decimal)11.1;
-            decimal lng = (decimal)1.6;
-            cmd.Parameters.AddWithValue("@lat", lat);
-            cmd.Parameters.AddWithValue("@lng", lng);
-            cmd.ExecuteNonQuery();
-
+                SqlCommand cmd = new SqlCommand("updateEvent");
+                cmd.Connection = sqlConnection;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eventID", row.EventID);
+                cmd.Parameters.AddWithValue("@name", row.Name);
+                cmd.Parameters.AddWithValue("@startDate", row.StartDate);
+                cmd.Parameters.AddWithValue("@endDate", row.EndDate);
+                cmd.Parameters.AddWithValue("@description", row.Description);
+                cmd.Parameters.AddWithValue("@price", row.Price);
+                cmd.Parameters.AddWithValue("@lunchPrice", row.LunchPrice);
+                cmd.Parameters.AddWithValue("@dinnerPrice", row.DinnerPrice);
+                cmd.Parameters.AddWithValue("@theme", row.Theme);
+                cmd.Parameters.AddWithValue("@postcode", row.Postcode);
+                cmd.Parameters.AddWithValue("@hint", row.Hint);
+                cmd.Parameters.AddWithValue("@fulldetails", row.FullDetails);
+                cmd.Parameters.AddWithValue("@duration", row.Duration);
+                cmd.Parameters.AddWithValue("@minPeople", row.MinPeople);
+                cmd.Parameters.AddWithValue("@maxPeople", row.MaxPeople);
+                //Google Maps Lang / Lat need to be added.
+                decimal lat = (decimal)11.1;
+                decimal lng = (decimal)1.6;
+                cmd.Parameters.AddWithValue("@lat", lat);
+                cmd.Parameters.AddWithValue("@lng", lng);
+                cmd.ExecuteNonQuery();
+            }
         }
 
         public void UpdateEventDetail(EventDetail row)
         {
-            SqlConnection sqlConnection = new SqlConnection(SqlconnectionString);
-            sqlConnection.Open();
+            using (SqlConnection sqlConnection = new SqlConnection(SqlconnectionString))
+            {
+                sqlConnection.Open();
 
-            SqlCommand cmd = new SqlCommand("UpdateEventDetail");
-            cmd.Connection = sqlConnection;
-            cmd.CommandType = CommandType.StoredProcedure;
+                SqlCommand cmd = new SqlCommand("UpdateEventDetail");
+                cmd.Connection = sqlConnection;
+                cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.AddWithValue("@eventDetailID", row.EventDetailID);
-            cmd.Parameters.AddWithValue("@eventID", row.EventID);
-            cmd.Parameters.AddWithValue("@dayOfWeek", row.DayOfWeek);
-            cmd.Parameters.AddWithValue("@startTime", row.StartTime);
-            cmd.Parameters.AddWithValue("@endTime", row.EndTime);
-            cmd.Parameters.AddWithValue("@allDay", row.allDay);
-            cmd.ExecuteNonQuery();
-
+                cmd.Parameters.AddWithValue("@eventDetailID", row.EventDetailID);
+                cmd.Parameters.AddWithValue("@eventID", row.EventID);
+                cmd.Parameters.AddWithValue("@dayOfWeek", row.DayOfWeek);
+                cmd.Parameters.AddWithValue("@startTime", row.StartTime);
+                cmd.Parameters.AddWithValue("@endTime", row.EndTime);
+                cmd.Parameters.AddWithValue("@allDay", row.allDay);
+                cmd.ExecuteNonQuery();
+            }
         }
 
     }
