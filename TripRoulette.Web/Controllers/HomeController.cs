a56b64076f286ab7e93978be13adcf8a0848e3dd
4noreason <mail@4noreason.com>
2014-11-22 15:42:19 +0000

    Design changes Signed-off-by: 4noreason <mail@4noreason.com>

--- TripRoulette.Web/Controllers/HomeController.cs
+++ TripRoulette.Web/Controllers/HomeController.cs
@@ -13,6 +13,20 @@ namespace TripRoulette.Web.Controllers
             return View();
         }
 
+        public ActionResult SendEmail(string numPeople, string leave, string budget, string leavingfrom)
+        {
+            /// Code here!!!!.....
+
+
+            return View();
+        }
+
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
